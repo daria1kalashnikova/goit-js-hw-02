cb7455d878bd74b0bbcfc09ff3eeccc10b64eb19
daria1kalashnikova <dk.kalashnikova@gmail.com>
2024-10-15 16:02:46 +0300

    tasks done

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,9 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
   if (totalPrice <= customerCredits) {
-    return `Insufficient funds!`;
-  } else {
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  } else {
+    return `Insufficient funds!`;
   }
 }
 
--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,12 @@
+function formatMessage(message, maxLength) {
+  if (message.length > maxLength) {
+    const messageMax = message.slice(0, maxLength) + "...";
+    return messageMax;
+  } else {
+    return message;
+  }
+}
+
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
 console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
